49061337f025cdd0184ad5d16f69a6e6239f5820
ThePugo <sashacammarata@gmail.com>
2024-05-15 11:34:14 +0200

    correccion

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -3,9 +3,6 @@ public static class EventManager
     public delegate void KeyEventHandler();
     public static event KeyEventHandler OnKeyCollected;
 
-    public delegate void DeathHandler();
-    public static event DeathHandler OnDeath;
-
     public static void KeyCollected()
     {
         if (OnKeyCollected != null)
